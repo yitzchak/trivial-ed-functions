8bb6531d4157f0fdfb88bea56a15ce9a818805e2
Tarn W. Burton <twburton@gmail.com>
2021-06-22 08:07:27 -0400

    Gracefully handle missing symbols

--- lisp/interface.lisp
+++ lisp/interface.lisp
@@ -1,11 +1,11 @@
 (in-package #:trivial-ed-functions)
 
 
-#-(or clasp ecl sbcl)
-(declaim (type list *ed-functions*))
-#-(or clasp ecl sbcl)
 (defvar *ed-functions* '()
-  "See function documentation for ED.")
+  "If supported by the current lisp implementation then functions from the
+list EXT:*ED-FUNCTIONS* are called in order with X as an argument until one
+of them returns non-NIL; these functions are responsible for signalling a
+FILE-ERROR to indicate failure to perform an operation on the file system.")
 
 
 (defun ed-hook (&optional x)
@@ -15,12 +15,20 @@
       (return t))))
 
 
-(defun initialize ()
-  "Initialize any needed hook functions to make *ed-functions* actually work."
-  #+allegro (setf scm::*ed-hook* #'ed-hook)
-  #+ccl (setf ccl:*resident-editor-hook* #'ed-hook)
-  (values))
+; Support implementations that have an existing *ED-FUNCTIONS*, but only add feature if actually present.
+#+(or abcl clasp ecl sbcl)
+(let ((sym (find-symbol "*ED-FUNCTIONS*" #+sbcl "SB-EXT" #+(or abcl clasp ecl) "EXT")))
+  (when sym
+    (shadowing-import sym "TRIVIAL-ED-FUNCTIONS")
+    (export (find-symbol "*ED-FUNCTIONS*" "TRIVIAL-ED-FUNCTIONS"))
+    (pushnew :ed-functions *features*)))
 
 
-(initialize)
+; Support implementations that have a single hook, but only add feature if actually present.
+#+(or allegro ccl)
+(let ((sym (find-symbol #+allegro "*ED-HOOK*" #+ccl "*RESIDENT-EDITOR-HOOK*"
+                        #+allegro "SCM" #+ccl "CCL")))
+  (when sym
+    (setf (symbol-value sym) #'ed-hook)
+    (pushnew :ed-functions *features*)))
 
--- lisp/packages.lisp
+++ lisp/packages.lisp
@@ -2,9 +2,6 @@
   (:use #:common-lisp)
   (:nicknames :tef)
   (:documentation "A simple compatibility layer for *ed-functions*")
-  #+(or clasp ecl) (:import-from #:ext #:*ed-functions*)
-  #+sbcl (:import-from #:sb-ext #:*ed-functions*)
   (:export
-    #:*ed-functions*
-    #:initialize))
+    #:*ed-functions*))
 
